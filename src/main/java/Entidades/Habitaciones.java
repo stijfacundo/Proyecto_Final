b870d2617ab60f2358b059389e1edf39c47a7799
Dany23924 <aguilardany@gmail.com>
2023-10-02 13:16:21 -0300

    primeros pasos para el final

--- src/main/java/Entidades/Habitaciones.java
+++ src/main/java/Entidades/Habitaciones.java
@@ -1,5 +1,78 @@
 package Entidades;
 
 public class Habitaciones {
+    int idHabitacion;
+    int nroHabitacion;
+    int piso;
+    boolean ocupada;
+    int idTipoHabitacion;
+
+    public Habitaciones() {
+    }
+
+    public Habitaciones(int nroHabitacion, int piso, boolean ocupada, int idTipoHabitacion) {
+        this.nroHabitacion = nroHabitacion;
+        this.piso = piso;
+        this.ocupada = ocupada;
+        this.idTipoHabitacion = idTipoHabitacion;
+    }
+
+    public Habitaciones(int idHabitacion, int nroHabitacion, int piso, boolean ocupada, int idTipoHabitacion) {
+        this.idHabitacion = idHabitacion;
+        this.nroHabitacion = nroHabitacion;
+        this.piso = piso;
+        this.ocupada = ocupada;
+        this.idTipoHabitacion = idTipoHabitacion;
+    }
+
+    public int getIdHabitacion() {
+        return idHabitacion;
+    }
+
+    public void setIdHabitacion(int idHabitacion) {
+        this.idHabitacion = idHabitacion;
+    }
+
+    public int getNroHabitacion() {
+        return nroHabitacion;
+    }
+
+    public void setNroHabitacion(int nroHabitacion) {
+        this.nroHabitacion = nroHabitacion;
+    }
+
+    public int getPiso() {
+        return piso;
+    }
+
+    public void setPiso(int piso) {
+        this.piso = piso;
+    }
+
+    public boolean isOcupada() {
+        return ocupada;
+    }
+
+    public void setOcupada(boolean ocupada) {
+        this.ocupada = ocupada;
+    }
+
+    public int getIdTipoHabitacion() {
+        return idTipoHabitacion;
+    }
+
+    public void setIdTipoHabitacion(int idTipoHabitacion) {
+        this.idTipoHabitacion = idTipoHabitacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Habitaciones{" + "idHabitacion=" + idHabitacion + 
+                ", nroHabitacion=" + nroHabitacion + ", piso=" + piso + 
+                ", ocupada=" + ocupada + ", idTipoHabitacion=" + 
+                idTipoHabitacion + '}';
+    }
     
+    
+
 }
--- src/main/java/Entidades/TipoHabitacion.java
+++ src/main/java/Entidades/TipoHabitacion.java
@@ -2,4 +2,103 @@ package Entidades;
 
 public class TipoHabitacion {
     
+    int idTipoHabitacion;
+    String codigo;
+    String nombre;
+    int capacidadMaxima;
+    int cantidadCamas;
+    String tipoCama;
+    double precioPorNoche;
+
+    public TipoHabitacion() {
+    }
+
+    public TipoHabitacion(String codigo, String nombre, int capacidadMaxima, int cantidadCamas, String tipoCama, double precioPorNoche) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.capacidadMaxima = capacidadMaxima;
+        this.cantidadCamas = cantidadCamas;
+        this.tipoCama = tipoCama;
+        this.precioPorNoche = precioPorNoche;
+    }
+
+    public TipoHabitacion(int idTipoHabitacion, String codigo, String nombre, int capacidadMaxima, int cantidadCamas, String tipoCama, double precioPorNoche) {
+        this.idTipoHabitacion = idTipoHabitacion;
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.capacidadMaxima = capacidadMaxima;
+        this.cantidadCamas = cantidadCamas;
+        this.tipoCama = tipoCama;
+        this.precioPorNoche = precioPorNoche;
+    }
+
+    public int getIdTipoHabitacion() {
+        return idTipoHabitacion;
+    }
+
+    public void setIdTipoHabitacion(int idTipoHabitacion) {
+        this.idTipoHabitacion = idTipoHabitacion;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    public void setCapacidadMaxima(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+    }
+
+    public int getCantidadCamas() {
+        return cantidadCamas;
+    }
+
+    public void setCantidadCamas(int cantidadCamas) {
+        this.cantidadCamas = cantidadCamas;
+    }
+
+    public String getTipoCama() {
+        return tipoCama;
+    }
+
+    public void setTipoCama(String tipoCama) {
+        this.tipoCama = tipoCama;
+    }
+
+    public double getPrecioPorNoche() {
+        return precioPorNoche;
+    }
+
+    public void setPrecioPorNoche(double precioPorNoche) {
+        this.precioPorNoche = precioPorNoche;
+    }
+
+    @Override
+    public String toString() {
+        return "TipoHabitacion{" + "idTipoHabitacion=" + idTipoHabitacion + 
+                ", codigo=" + codigo + ", nombre=" + nombre + 
+                ", capacidadMaxima=" + capacidadMaxima + 
+                ", cantidadCamas=" + cantidadCamas + 
+                ", tipoCama=" + tipoCama + ", precioPorNoche=" 
+                + precioPorNoche + '}';
+    }
+    
+    
+    
+    
 }
