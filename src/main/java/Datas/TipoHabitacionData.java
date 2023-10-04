de6651e3ca587296e48214697e8954ceadbce61c
enriquezleandro <enriquezleandro@gmail.com>
2023-10-04 12:27:20 -0300

    realizacion metodo buscquedaDeHabitacionPorNombre

--- src/main/java/Datas/TipoHabitacionData.java
+++ src/main/java/Datas/TipoHabitacionData.java
@@ -12,14 +12,14 @@ public class TipoHabitacionData {
     }
 
     public void busquedaDeHabitacionPorCapacidad(int capacidadMaxima) throws SQLException {
-        // Buscamos el tipo de habitacion acorde a la capacidad solicitada
+        // Buscamos el tipo de habitacion acorde a su capacidad maxima
         String sql = "SELECT id_tipo_habitacion, codigo, nombre, capacidadMaxima, cantidadCamas, tipoCamas, PrecioPorNoche FROM tipo_habitacion WHERE capacidadMaxima >= ?";
         TipoHabitacion tipoHabitacion = null;
         try (PreparedStatement ps = con.prepareStatement(sql)) {
             ps.setInt(1, capacidadMaxima);
             ResultSet rs = ps.executeQuery();
 
-            // Guardamos en un Alumno los datos obtenidos
+            // Guardamos en un TipoHabitacion los datos obtenidos
             if (rs.next()) {
                 tipoHabitacion = new TipoHabitacion();
                 tipoHabitacion.setIdTipoHabitacion(rs.getInt("id_tipo_habitacion"));
@@ -31,6 +31,26 @@ public class TipoHabitacionData {
                 tipoHabitacion.setPrecioPorNoche(rs.getDouble("PrecioPorNoche"));
             }
         }
+    }
+        public void busquedaDeHabitacionPorNombre(String nombre) throws SQLException {
+        // Buscamos el tipo de habitacion acorde por nombre
+        String sql = "SELECT id_tipo_habitacion, codigo, nombre, capacidadMaxima, cantidadCamas, tipoCamas, PrecioPorNoche FROM tipo_habitacion WHERE nombre = ?";
+        TipoHabitacion tipoHabitacion = null;
+        try (PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, nombre);
+            ResultSet rs = ps.executeQuery();
 
+            // Guardamos en un TipoHabitacion los datos obtenidos
+            if (rs.next()) {
+                tipoHabitacion = new TipoHabitacion();
+                tipoHabitacion.setIdTipoHabitacion(rs.getInt("id_tipo_habitacion"));
+                tipoHabitacion.setCodigo(rs.getString("codigo"));
+                tipoHabitacion.setNombre(rs.getString("nombre"));
+                tipoHabitacion.setCapacidadMaxima(rs.getInt("capacidadMaxima"));
+                tipoHabitacion.setCantidadCamas(rs.getInt("cantidadCamas"));
+                tipoHabitacion.setTipoCama(rs.getString("tipoCamas"));
+                tipoHabitacion.setPrecioPorNoche(rs.getDouble("PrecioPorNoche"));
+            }
+        }
     }
 }
--- src/main/java/Vistas/Inicio.java
+++ src/main/java/Vistas/Inicio.java
@@ -32,11 +32,11 @@ public class Inicio extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 929, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 581, Short.MAX_VALUE)
         );
 
         pack();
