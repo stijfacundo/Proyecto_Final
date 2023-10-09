84d9c17c49694e97be0b2f9ea10313b7cd9a5225
stijfacundo <106571245+stijfacundo@users.noreply.github.com>
2023-10-09 12:23:29 -0300

    InicioTesting actualizada

--- src/main/java/Vistas/InicioTesting.java
+++ src/main/java/Vistas/InicioTesting.java
@@ -81,6 +81,7 @@ public class InicioTesting extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jmiGestionarReservasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmiGestionarReservasActionPerformed
